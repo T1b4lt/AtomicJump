4ee416fc0b133027c51041d72cf2bc6eda72a2b0
Guillermo Segovia <T1b4lt@users.noreply.github.com>
2024-07-14 14:47:42 +0200

    feat: :sparkles: add seed mode

--- screens/main_menu/main_menu.gd
+++ screens/main_menu/main_menu.gd
@@ -4,6 +4,9 @@ extends CanvasLayer
 # Scenes
 const LEVEL_SCENE = "res://screens/level/level.tscn"
 
+# Childs
+@onready var game_seed_input = $VBoxContainer/HBoxContainer/game_seed_input
+
 func _on_solo_mode_button_pressed():
 	# Generate a game seed
 	var game_seed = str(Time.get_unix_time_from_system()).hash()
@@ -11,6 +14,16 @@ func _on_solo_mode_button_pressed():
 	# Change to scene level
 	get_tree().change_scene_to_file(LEVEL_SCENE)
 
+func _on_seed_mode_button_pressed():
+	# Check that game seed is correct
+	if game_seed_input.text.length() != 9 or !game_seed_input.text.is_valid_int():
+		# Reset input
+		game_seed_input.text = ""
+		return
+	# Set global game seed
+	global.set_seed(int(game_seed_input.text))
+	# Change to scene level
+	get_tree().change_scene_to_file(LEVEL_SCENE)
 
 func _on_exit_button_pressed():
 	# Exit game
